2d5a1b984126d4915ee0c29ee8f75477c6cd02fa
Alan-GG117 <alanino05@hotmail.com>
2026-01-12 17:15:58 -0600

    Ajuste de barra de navegacion

--- src/app/shared/navbar/navbar.ts
+++ src/app/shared/navbar/navbar.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {
+export class Navbar implements OnInit {
+  usuario: any = null;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.verificarSesion();
+  }
+
+  verificarSesion() {
+    const sesion = localStorage.getItem('usuario_sesion');
+    if (sesion) {
+      this.usuario = JSON.parse(sesion);
+    } else {
+      this.usuario = null;
+    }
+  }
+
+  cerrarSesion() {
+    localStorage.removeItem('usuario_sesion');
+    this.usuario = null;
+    this.router.navigate(['/login']);
+  }
 }
